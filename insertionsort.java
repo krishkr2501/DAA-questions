87df5a4e743958fe5840f4720b3028670c06d959
KRISH KUMAR <sahaykrish1526@outlook.com>
2025-04-04 10:08:30 +0530

    Add files via upload

---
+++ insertionsort.java
@@ -0,0 +1,17 @@
+public class insertionsort {
+    public static void main(String[] args){
+            int[] arr= {45,4,78,12,5};
+            for(int i=1;i<arr.length; i++){
+                int current = arr[i];
+                int j=i-1;
+                while(j>=0 && current<arr[j]){
+                    arr[j+1]= arr[j];
+                    j--;
+                }
+                arr[j+1]= current;
+            }
+            for (int i=0; i<arr.length; i++){
+                System.out.print(arr[i]+" ");
+            }
+    }
+}
